88c60927396113e15c2af765d128abd7c1b17242
Tatas Fachrul <tatasfachrul@gmail.com>
2019-08-15 16:08:09 +0700

    global config

---
+++ src/configs/configs.js
@@ -0,0 +1,23 @@
+require('dotenv/config')
+
+module.exports = {
+  database: {
+    mysql: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    postgres: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      schema: 'public',
+      driver: 'pg',
+      multipleStatements: true
+    }
+  },
+  port: process.env.PORT
+}
