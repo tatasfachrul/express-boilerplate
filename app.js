a93c03ed28ad6be75fd211694ae32d8fd3b3f702
Tatas Fachrul <tatasfachrul@gmail.com>
2019-08-15 13:38:12 +0700

    Post method

--- app.js
+++ app.js
@@ -15,4 +15,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
   res.send('Created endpoint!')
 })
+
+app.post('/', (req, res) => {
+  const body = req.body.string
+
+  res.send(body)
+})
 module.exports = app
