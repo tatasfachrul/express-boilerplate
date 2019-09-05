bf36cfbae62bc0a1737957caf4849e5653d04a71
Tatas Fachrul <tatasfachrul@gmail.com>
2019-09-05 13:45:33 +0700

    add auth route

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const Route = express.Router()
 const products = require('./routes/products')
 const categories = require('./routes/categories')
 const uploads = require('./routes/uploads')
+const auth = require('./routes/auth')
 
 Route
   .use('/products', products)
   .use('/categories', categories)
   .use('/uploads', uploads)
+  .use('/auth', auth)
 
 module.exports = Route
---
+++ src/routes/auth.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const Route = express.Router()
+
+Route
+  .post('/')
+  .post('/register')
+
+module.exports = Route
